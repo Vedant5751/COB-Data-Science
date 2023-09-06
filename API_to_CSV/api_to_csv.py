268e088012d1b73a58ead80577930e198098afce
Vedant5751 <104258810+Vedant5751@users.noreply.github.com>
2023-09-06 17:28:03 +0530

    Add files via upload

--- API_to_CSV/api_to_csv.py
+++ API_to_CSV/api_to_csv.py
@@ -1,1 +1,10 @@
-
+import pandas as pd
+import requests
+api_url = 'https://data.binance.com/api/v3/ticker/24hr'  
+response = requests.get(api_url)
+if response.status_code == 200:
+    api_data = response.json()  
+else:
+    print("Failed to retrieve data from the API")
+df = pd.DataFrame(api_data)
+df.to_csv('api_data.csv', index=False)
